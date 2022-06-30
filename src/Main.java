c21d1cea93a617d9edc5d6d76bcb1a4013e671f9
raziya1520 <inaatnurmatova72@gmail.com>
2022-06-30 13:00:18 +0600

    lesson3

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,28 @@ import java.util.Random;
 import java.util.concurrent.LinkedTransferQueue;
 
 public class Main{
-    public static void main(String[]args){
-        double[] arrey1 = {5.8,6.7,3.1,-6.9,2.6,-9.3,-6.5,9.5,-3.1,8.2,9.1,-6.1,3.5,-6.4,9.1};
+    public static void main(String[] args){
+        double[] arrey1 = {1.3,6.4,-9.4,6.3,-9.2,26.6,-5.1,1.5,-3.4,-7.8,-1.3,6.8,-2.4,6.2,-4.5,};
         int index = 0;
         double sum = 0.0;
         boolean on = false;
-       for (double summedNumbers : arrey1){
-           if (summedNumbers < 0 ){
-               on = true;
-           } else if (on == true){
-               sum += summedNumbers;
-               index++;
-           }
-       }
-       double number = sum / index;
+        for (double sumedNumbers : arrey1){
+            if (sumedNumbers < 0 ){
+                on = true;
+            }else  if (on == true){
+                sum += sumedNumbers;
+                index++;
+            }
+        }
+        double number = sum / index;
         System.out.println(index);
         System.out.println(sum);
         System.out.println("my answer" + number);
-
     }
+
+
+
+
 
 
 
