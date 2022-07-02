76ab338e0598dd84f9c9e831ef1379c4672cfa8c
raziya1520 <inaatnurmatova72@gmail.com>
2022-07-02 21:58:07 +0600

    lesson5

---
+++ src/Boss.java
@@ -0,0 +1,30 @@
+public class Boss {
+    public int health;
+    public int hit;
+    public String protection;
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public int getHit(){
+        return hit;
+    }
+
+    public void setHit(int hit){
+        this.hit = hit;
+    }
+
+    public String getProtection(){
+        return protection;
+    }
+
+    public void setProtection(String protection){
+        this.protection = protection;
+    }
+
+}
